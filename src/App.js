b40647a072565df9a7564a8af28d10de59460703
Renier Vargas <renier1989@gmail.com>
2023-08-10 15:37:47 -0500

    Class77 - Componente Card

--- src/App.js
+++ src/App.js
@@ -1,32 +1,37 @@
+import Card from "./components/Card";
 import Input from "./components/Input";
 import useFormulario from "./hooks/useFormulario";
 
 function App() {
   // custom hook
-  const [formulario, handleChange] = useFormulario({ 
-      name: ''
-      , lastname:'' 
-    });
+  const [formulario, handleChange] = useFormulario({
+    name: "",
+    lastname: "",
+  });
 
   return (
-    <div>
-      <form>
-        <Input
-        label='Nombre de Usuario'
-        type="text"
-        name="name"
-        placeholder="Nombre" 
-        value={formulario.name} 
-        onChange={handleChange} />
-        
-        <Input
-        label='Apellido de Usuario'
-        type="text"
-        name="lastname"
-        placeholder="Apellido" 
-        value={formulario.lastname} 
-        onChange={handleChange} />
-      </form>
+    <div className="h-screen bg-stone-300">
+      <Card>
+        <form>
+          <Input
+            label="Nombre de Usuario"
+            type="text"
+            name="name"
+            placeholder="Nombre"
+            value={formulario.name}
+            onChange={handleChange}
+          />
+
+          <Input
+            label="Apellido de Usuario"
+            type="text"
+            name="lastname"
+            placeholder="Apellido"
+            value={formulario.lastname}
+            onChange={handleChange}
+          />
+        </form>
+      </Card>
     </div>
   );
 }
---
+++ src/components/Card.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Card({ children }) {
+  return <div className="bg-white">{children}</div>;
+}
+
+export default Card;
